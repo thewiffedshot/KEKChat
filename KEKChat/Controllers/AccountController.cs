badb02f77769f3b5371a63b36b34b9e426982537
Simeon Laplev <thewiffedshot@gmail.com>
2019-02-25 15:44:34 +0200

    Initial commit.

---
+++ KEKChat/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KEKChat.Models;
+
+namespace KEKChat.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        //[HttpPost]
+        public ActionResult Login()
+        {
+           
+            return View();
+        }
+
+        //[HttpPost]
+        public ActionResult Register()
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ KEKChat/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KEKChat.MvcApplication" Language="C#" %>
